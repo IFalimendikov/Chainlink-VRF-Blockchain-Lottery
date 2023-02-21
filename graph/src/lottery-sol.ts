4aeecc7773c62f2a5ea0702247da441098f56b3f
Ivan Falimendikov <golovanjot2@gmail.com>
2023-02-21 13:21:01 +0100

    Init DApp

--- graph/src/lottery-sol.ts
+++ graph/src/lottery-sol.ts
@@ -1,72 +1,60 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
-  GameEnded as GameEndedEvent,
-  GameStarted as GameStartedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  PlayerJoined as PlayerJoinedEvent
-} from "../generated/Lottery.sol/Lottery.sol"
-import {
+  PlayerJoined,
   GameEnded,
   GameStarted,
   OwnershipTransferred,
-  PlayerJoined
-} from "../generated/schema"
+} from "../generated/Lottery.sol/Lottery.sol";
+import { Game } from "../generated/schema";
 
-export function handleGameEnded(event: GameEndedEvent): void {
-  let entity = new GameEnded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
-  entity.winner = event.params.winner
-  entity.requestId = event.params.requestId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function handleGameEnded(event: GameEnded): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Game.load(event.params.gameId.toString());
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    return;
+  }
+  // Entity fields can be set based on event parameters
+  entity.winner = event.params.winner;
+  entity.requestId = event.params.requestId;
+  // Entities can be written to the store with `.save()`
+  entity.save();
 }
 
-export function handleGameStarted(event: GameStartedEvent): void {
-  let entity = new GameStarted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
-  entity.maxPlayers = event.params.maxPlayers
-  entity.entryFee = event.params.entryFee
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function handlePlayerJoined(event: PlayerJoined): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Game.load(event.params.gameId.toString());
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    return;
+  }
+  // Entity fields can be set based on event parameters
+  let newPlayers = entity.players;
+  newPlayers.push(event.params.player);
+  entity.players = newPlayers;
+  // Entities can be written to the store with `.save()`
+  entity.save();
 }
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function handleGameStarted(event: GameStarted): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Game.load(event.params.gameId.toString());
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new Game(event.params.gameId.toString());
+    entity.players = [];
+  }
+  // Entity fields can be set based on event parameters
+  entity.maxPlayers = event.params.maxPlayers;
+  entity.entryFee = event.params.entryFee;
+  // Entities can be written to the store with `.save()`
+  entity.save();
 }
 
-export function handlePlayerJoined(event: PlayerJoinedEvent): void {
-  let entity = new PlayerJoined(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
-  entity.player = event.params.player
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
